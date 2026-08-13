8ace6bdb246e92e3272a6734dbaaacac1da06983
EnlightenedSnake <jakemikruk@gmail.com>
2026-08-13 15:14:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,9 +6,9 @@
       MP = document.getElementById('mainPage'),
       FP = document.getElementById('fightPage'),
       IP = document.getElementById('inventoryPage'),
-    	SPU = document.getElementById('statsPopUp'),
-  	openSPUBtn = document.getElementById('statsButton'),
-  	closeSPUBtn = document.getElementById('exitStatsPopUpButton'),
+      SPU = document.getElementById('statsPopUp'),
+  	  openSPUBtn = document.getElementById('statsButton'),
+  	  closeSPUBtn = document.getElementById('exitStatsPopUpButton'),
       incStatBtn = document.getElementsByClassName('incStat'),
       stats = document.getElementsByClassName('stat'),
       statPoints = document.getElementById('statPoints'),
@@ -23,15 +23,18 @@
       incStageLvl = document.getElementById('increaseStageLevel'),
       decStageLvl = document.getElementById('decreaseStageLevel'),
       MPHeroTable = document.getElementById('mainPageHeroTable'),
-      FPEneHealthBar = document.getElementById('fightPageEnemyHealthBar'),
+      MPHeroXPBar = MPHeroTable.children[0].children[1].children[1].children[0].getContext("2d"),
+      FPEneHealthBarCanvas = document.getElementById('fightPageEnemyHealthBar'),
+      FPEneHealthBar = FPEneHealthBarCanvas.getContext("2d"),
       FPEneHealth = document.getElementById('fightPageEnemyHealth'),
       FPEneImg = document.getElementById('fightPageEnemyImage'),
       FPHeroHealth = document.getElementById('fightPageHeroHealth'),
-      FPHeroHealthBar = document.getElementById('fightPageHeroHealthBar'),
+      FPHeroHealthBarCanvas = document.getElementById('fightPageHeroHealthBar'),
+      FPHeroHealthBar = FPHeroHealthBarCanvas.getContext("2d"),
       attackBtn = document.getElementById('attackButton'),
       itemsBtn = document.getElementById('itemsButton'),
       battleLog = document.getElementById('battleLog');
-  
+
 class Equipment {
   name = "Sword";
   type = "Helmet, Chestplate, Pants, Boots, Weapon, Shield, or Healing";
@@ -46,6 +49,8 @@ class Equipment {
 }
 const flimsyLeatherBoots = new Equipment("Leather Boots that are holding on by a thread, +1 defense", "Boots", 1, "");
 
+const equipmentList = [flimsyLeatherBoots];
+  
 class Enemy {
   name = "Joe";
   health = 0;
@@ -66,8 +71,10 @@ class Enemy {
     this.image = img;
   }
 }
- const firstEnemy = new Enemy("Weak",5,2,0,2, [flimsyLeatherBoots],[0.3],"");
+const firstEnemy = new Enemy("Weak",5,2,0,2, [flimsyLeatherBoots],[0.3],"");
 
+const enemyList = [firstEnemy];
+  
 var heroMaxHealth = 10;
 var heroDamage = 2;
 var heroDefense = 0;
@@ -79,7 +86,8 @@ var heroArmorInven = [];
 var heroWeaponInven = [];
 var heroHealInven = [];
 var currentStage = 0;
-
+var enemyCurrentHealth = 0;
+var heroCurrentHealth = 0;
 
 function showItem(i){
 	if (i<10) {
@@ -112,9 +120,60 @@ function showItem(i){
     }
   }
 }
-
-
-
+function updateHealthBars() {
+  FPEneHealthBar.reset();
+  FPHeroHealthBar.reset();
+	FPEneHealthBar.beginPath();
+	FPEneHealthBar.fillStyle = "gray";  	
+  FPEneHealthBar.roundRect(0,0,0.6*window.innerWidth,0.29*window.innerHeight,0.025*window.innerHeight);
+  FPEneHealthBar.fill();
+  FPEneHealthBar.closePath();
+  FPEneHealthBar.beginPath();
+	FPEneHealthBar.fillStyle = "red";
+  FPEneHealthBar.roundRect(0,0.0,(enemyCurrentHealth/enemyList[currentStage].health)*0.6*window.innerWidth,0.29*window.innerHeight,0.025*window.innerHeight);
+  FPEneHealthBar.fill();
+  FPEneHealthBar.closePath();
+  FPHeroHealthBar.beginPath();
+	FPHeroHealthBar.fillStyle = "gray";  	
+  FPHeroHealthBar.roundRect(0,0,0.6*window.innerWidth,0.29*window.innerHeight,0.025*window.innerHeight);
+  FPHeroHealthBar.fill();
+  FPHeroHealthBar.closePath();
+  FPHeroHealthBar.beginPath();
+	FPHeroHealthBar.fillStyle = "red";
+  FPHeroHealthBar.roundRect(0,0,(heroCurrentHealth/heroMaxHealth)*0.6*window.innerWidth,0.29*window.innerHeight,0.025*window.innerHeight);
+  FPHeroHealthBar.fill();
+  FPHeroHealthBar.closePath();
+}
+function startFightPage() {
+  heroCurrentHealth = heroMaxHealth;
+  enemyCurrentHealth = enemyList[currentStage].health;
+	FPEnHealth = enemyCurrentHealth + "/" + enemyCurrentHealth + " HP";
+  FPEneHealthBar.beginPath();
+	FPEneHealthBar.fillStyle = "gray";  	
+  FPEneHealthBar.roundRect(0,0,0.6*window.innerWidth,0.29*window.innerHeight,0.025*window.innerHeight);
+  FPEneHealthBar.fill();
+	FPEneHealthBar.fillStyle = "red"; 	
+  FPEneHealthBar.roundRect(0.025*window.innerWidth,0.025*window.innerHeight,0.55*window.innerWidth,0.24*window.innerHeight,0.025*window.innerHeight);
+  FPEneHealthBar.fill();
+  FPHeroHealthBar.beginPath();
+	FPHeroHealthBar.fillStyle = "gray";
+  FPHeroHealthBar.roundRect(0,0,0.6*window.innerWidth,0.29*window.innerHeight,0.025*window.innerHeight);
+  FPHeroHealthBar.fill();
+	FPHeroHealthBar.fillStyle = "red";
+  FPHeroHealthBar.roundRect(0.025*window.innerWidth,0.025*window.innerHeight,0.55*window.innerWidth,0.24*window.innerHeight,0.025*window.innerHeight);
+  FPHeroHealthBar.fill();
+  
+}
+function checkWinConditions(){
+	if(heroCurrentHealth == 0){
+  	battleLog.innerText += "\nYou died, press any key to go to Main Page"
+    document.onkeydown = () => {
+    	FP.style.display = "none";
+    	MP.style.display = "flex";
+      battleLog.innerText = "It's time to d-d-d-d-d-d-duel";
+    }
+  }
+}
 
 IPtoMPBtn.onclick = () => {
     IP.style.display = "none";
@@ -127,6 +186,7 @@ MPtoIPBtn.onclick = () => {
 MPtoFPBtn.onclick = () => {
     MP.style.display = "none";
     FP.style.display = "flex";
+		startFightPage();
 }
 FPtoMPBtn.onclick = () => {
     FP.style.display = "none";
@@ -138,7 +198,13 @@ openSPUBtn.onclick = () => {
 closeSPUBtn.onclick = () => {
     SPU.style.display = "none";
 }
-
+attackBtn.onclick = () => {
+		enemyCurrentHealth -= heroDamage;
+  	heroCurrentHealth -= enemyList[currentStage].damage;
+  	battleLog.innerText += "\nHero has dealt " + heroDamage + " damage to " + enemyList[currentStage].name + "\n" + enemyList[currentStage].name + " has dealt " + enemyList[currentStage].damage + " damage to Hero"
+  	updateHealthBars();
+  	checkWinConditions();
+}
 for (let i = 0; i < 30; ++i) {
   inventory[i].mouseover = () => {
   	showItem(i);
@@ -148,4 +214,4 @@ for (let i = 0; i < 6; ++i) {
  	equippedImgs[i].mouseover = () => {
    	showItem(i+30); 
   }
-}  
+}
